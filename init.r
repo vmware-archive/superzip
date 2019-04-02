3be10a25360f70d37c4efc79f5d0ebae652783eb
wjjung317 <wjjung317@gmail.com>
2019-04-02 11:45:09 +0900

    Delete init.r

--- init.r
+++
@@ -1,9 +0,0 @@
-install.packages("shiny", dependencies = TRUE)
-install.packages("leaflet", dependencies = TRUE)
-install.packages("RColorBrewer", dependencies = TRUE)
-install.packages("scales", dependencies = TRUE)
-install.packages("lattice", dependencies = TRUE)
-install.packages("dplyr", dependencies = TRUE)
-install.packages("DT", dependencies = TRUE)
-
-options(device='cairo')
